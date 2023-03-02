f50f47c7cf006f11f06b21f3b969cea56f295312
Hyeonae <aeae_1@naver.com>
2023-03-02 18:17:54 +0900

    Feat: /education/quiz API 구현

--- src/main/java/com/mentionall/cpr2u/education/controller/EducationController.java
+++ src/main/java/com/mentionall/cpr2u/education/controller/EducationController.java
@@ -3,35 +3,67 @@ package com.mentionall.cpr2u.education.controller;
 import com.mentionall.cpr2u.education.service.EducationProgressService;
 import com.mentionall.cpr2u.education.service.LectureService;
 import com.mentionall.cpr2u.education.service.QuizService;
+import com.mentionall.cpr2u.util.ResponseDataTemplate;
 import com.mentionall.cpr2u.util.ResponseTemplate;
 import com.mentionall.cpr2u.util.exception.ResponseCode;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/education")
 @RequiredArgsConstructor
-public class EducationController {
+public class  EducationController {
     private final EducationProgressService progressService;
     private final LectureService lectureService;
     private final QuizService quizService;
 
     @GetMapping()
     public ResponseEntity<ResponseTemplate> getEducationInfo() {
+        // TODO: 비즈니스 로직 구현
+
         return ResponseTemplate.toResponseEntity(ResponseCode.OK);
     }
 
     @GetMapping("/lecture")
     public ResponseEntity<ResponseTemplate> getLectureList() {
+        // TODO: 비즈니스 로직 구현
+
         return ResponseTemplate.toResponseEntity(ResponseCode.OK);
     }
 
-    @GetMapping("/lecture/progress/{lectureId}")
-    public ResponseEntity<ResponseTemplate> takeLecture(@PathVariable Long lectureId) {
+    @PostMapping("/lecture/progress/{lectureId}")
+    public ResponseEntity<ResponseTemplate> completeLecture(@PathVariable Long lectureId) {
+        // TODO: 비즈니스 로직 구현
+
+        return ResponseTemplate.toResponseEntity(ResponseCode.OK);
+    }
+
+    @GetMapping("/quiz")
+    public ResponseEntity<ResponseDataTemplate> getQuizList() {
+        return ResponseDataTemplate.toResponseEntity(
+                ResponseCode.OK,
+                quizService.readRandomQuizList(5));
+    }
+
+    @PostMapping("/quiz/progress")
+    public ResponseEntity<ResponseTemplate> completeQuiz() {
+        // TODO: 비즈니스 로직 구현
+
+        return ResponseTemplate.toResponseEntity(ResponseCode.OK);
+    }
+
+    @GetMapping("/posture")
+    public ResponseEntity<ResponseTemplate> getPostureLecture() {
+        // TODO: 비즈니스 로직 구현
+
+        return ResponseTemplate.toResponseEntity(ResponseCode.OK);
+    }
+
+    @PostMapping("/posture/progress")
+    public ResponseEntity<ResponseTemplate> completePosture() {
+        // TODO: 비즈니스 로직 구현
+
         return ResponseTemplate.toResponseEntity(ResponseCode.OK);
     }
 }
--- src/main/java/com/mentionall/cpr2u/education/service/QuizService.java
+++ src/main/java/com/mentionall/cpr2u/education/service/QuizService.java
@@ -1,12 +1,25 @@
 package com.mentionall.cpr2u.education.service;
 
+import com.mentionall.cpr2u.education.dto.QuizDto;
 import com.mentionall.cpr2u.education.repository.QuizRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class QuizService {
 
     private final QuizRepository quizRepository;
+
+    public List<QuizDto> readRandomQuizList(int count) {
+        List<QuizDto> response = new ArrayList();
+
+        quizRepository.findRandom(count).stream()
+                .map(q -> response.add(new QuizDto(q)));
+        return response;
+    }
 }
