4ce23199bea236870914388c7a4ecc6eb33bf5b0
Hyeonae <aeae_1@naver.com>
2023-03-09 20:49:41 +0900

    Feat: Call, Dispatch 엔티티 추가

--- src/main/java/com/mentionall/cpr2u/call/domain/Dispatch.java
+++ src/main/java/com/mentionall/cpr2u/call/domain/Dispatch.java
@@ -1,4 +1,38 @@
 package com.mentionall.cpr2u.call.domain;
 
+import com.mentionall.cpr2u.user.domain.User;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@NoArgsConstructor
+@EntityListeners(AuditingEntityListener.class)
 public class Dispatch {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "dispatcher_id")
+    private User dispatcher;
+
+    @Column
+    @Enumerated(EnumType.STRING)
+    private DispatchStatus statuc;
+
+    @CreatedDate
+    private LocalDateTime dispatchedAt;
+
+    @Column
+    private LocalDateTime arrivedAt;
+
+
+
 }
--- src/main/java/com/mentionall/cpr2u/call/service/CallService.java
+++ src/main/java/com/mentionall/cpr2u/call/service/CallService.java
@@ -1,4 +1,7 @@
 package com.mentionall.cpr2u.call.service;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public class CallService {
 }
