3ce60de3ff9b33b9f8070c0a49485d9348a2ecdd
YEJIN <yejon9858@gmail.com>
2023-03-11 02:45:49 +0900

    Merge branch 'develop' into fix/12-auth-user

--- src/main/java/com/mentionall/cpr2u/manager/ManagerController.java
+++ src/main/java/com/mentionall/cpr2u/manager/ManagerController.java
@@ -1,12 +1,10 @@
 package com.mentionall.cpr2u.manager;
 
-import com.mentionall.cpr2u.education.dto.quiz.OXQuizRequestDto;
-import com.mentionall.cpr2u.education.dto.quiz.SelectionQuizRequestDto;
+import com.mentionall.cpr2u.education.dto.quiz.QuizRequestDto;
 import com.mentionall.cpr2u.education.service.QuizService;
 import com.mentionall.cpr2u.util.ResponseTemplate;
 import com.mentionall.cpr2u.util.exception.ResponseCode;
 import io.swagger.v3.oas.annotations.Hidden;
-import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
@@ -24,15 +22,10 @@ public class ManagerController {
 
     private final QuizService quizService;
 
-    @PostMapping("/quizzes/ox")
-    public ResponseEntity<ResponseTemplate> createOxQuiz(@RequestBody OXQuizRequestDto requestDto) {
-        quizService.createOXQuiz(requestDto);
+    @PostMapping("/quizzes")
+    public ResponseEntity<ResponseTemplate> createQuiz(@RequestBody QuizRequestDto requestDto) {
+        quizService.createQuiz(requestDto);
         return ResponseTemplate.toResponseEntity(ResponseCode.OK);
     }
 
-    @PostMapping("/quizzes/selection")
-    public ResponseEntity<ResponseTemplate> createSelectionQuiz(@RequestBody SelectionQuizRequestDto requestDto) {
-        quizService.createSelectionQuiz(requestDto);
-        return ResponseTemplate.toResponseEntity(ResponseCode.OK);
-    }
 }
