f3cce06e753c453d5214557585023b091e98910e
YEJIN <yejon9858@gmail.com>
2023-03-18 03:08:21 +0900

    Fix: Mysql에 적용이 되지 않는 코드 수정

--- src/main/java/com/mentionall/cpr2u/education/repository/QuizRepository.java
+++ src/main/java/com/mentionall/cpr2u/education/repository/QuizRepository.java
@@ -3,12 +3,12 @@ package com.mentionall.cpr2u.education.repository;
 import com.mentionall.cpr2u.education.domain.Quiz;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.querydsl.QuerydslPredicateExecutor;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
-public interface QuizRepository extends JpaRepository<Quiz, Long> {
-    @Query(nativeQuery = true, value = "SELECT * FROM Quiz q ORDER BY RAND() LIMIT 5")
-    List<Quiz> findRandomLimit5();
+public interface QuizRepository extends JpaRepository<Quiz, Long>, QuizDslRepository, QuerydslPredicateExecutor<Quiz> {
+
 }
