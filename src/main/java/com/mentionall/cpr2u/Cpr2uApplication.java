8c4331527c66fd121ab100c62c338d96adaa2729
Kim Yejin <63632349+yejin9858@users.noreply.github.com>
2023-05-31 22:55:01 +0900

    Feat : TimeZone KST로 설정 코드 추가

--- src/main/java/com/mentionall/cpr2u/Cpr2uApplication.java
+++ src/main/java/com/mentionall/cpr2u/Cpr2uApplication.java
@@ -5,6 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
+import java.util.TimeZone;
+
 @EnableJpaAuditing
 @SpringBootApplication
 @EnableScheduling
